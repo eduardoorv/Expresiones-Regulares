1918dbc23853f613d79bd16da5eb386ecf7f81fd
natorro <carlos.nataren@gmail.com>
2016-03-08 11:20:14 -0600

    Added some functionality and changes to indentation

--- expresionesRegulares.R
+++ expresionesRegulares.R
@@ -1,15 +1,21 @@
-con<-file("CIJ_Total-ORIGINAL (1).res","r")
-s<-readLines(con,n=-1)
+setwd("/Users/natorro/Desktop/RFONDEN/githubs/eduardo/Expresiones-Regulares")
+conn <- file("CIJ_Total-ORIGINAL.res", "r") 
 
-y<-gsub("\\s+(\\d+)\\s+(\\d+)\\s+([^=]*=\\d*\\.*\\d*|[^_]+_[^\\s]*)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s*$","\\1,\\2,\\3,\\4,\\5,\\6,\\7,\\8,\\9",x=s,perl=T)
-y
+text_lines <- readLines(conn, n = -1)
 
-m<-array(dim = c(length(y),9))
+y <- gsub("\\s+(\\d+)\\s+(\\d+)\\s+([^=]*=\\d*\\.*\\d*|[^_]+_[^\\s]*)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s*$",
+          "\\1-\\2, \\3, \\4, \\5, \\6, \\7, \\8, \\9",
+          x = text_lines,
+          perl = TRUE)
+close(conn)
+head(y)
+tail(y)
+
+val_matrix <- array(dim = c(length(y), 9l))
 
 for(i in 1:length(y)){
-  m[i,]<-unlist(strsplit(x=y[i], split = ","))
+  val_matrix[i,] <- unlist(strsplit(x = y[i], split = ","))
 }
 
-m<-data.frame(m)
-
+val_dataframe <- data.frame(val_matrix)
 
